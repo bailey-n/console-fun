5db348c63eaeb5ef52f1b52cc054ad57aba21c8a
Kudeau <CloneUndefined@gmail.com>
2025-01-06 17:39:10 -0500

    Handle invalid regex searches

--- main.py
+++ main.py
@@ -1,28 +1,45 @@
 from __future__ import annotations
 
 import asyncio
-from typing import List
+import time
+from typing import List, Optional
 import colorama
 import keyboard
 import re
+from enum import Enum
 
 
 class SearchBar:
-    def __init__(self, options: List[str]):
+    class Mode(Enum):
+        Empty = 0
+        Escaping = 1
+        InvalidMode = 2
+        Standard = 3
+        Regex = 4
+        RegexInvalid = 5
+
+    def __init__(self, options: List[str], max_options_per_page: int = 50):
         self.line_count = len(options)
         self.options: List[str] = options
         self.printed_options: List[str] = []
         self.options_slice: List[str] = options
         self.running = False
-        self.search_line = ""
-        self.cursor = 0
+        self.search_line: str = ""
+        self.cursor: int = 0
+        self.max_opts: int = max_options_per_page
+        self.flair_text: Optional[str] = None
+        self.flair_color: colorama.Fore = colorama.Fore.WHITE
+        self.search_text_color: colorama.Fore = colorama.Fore.WHITE
+        self.mode: SearchBar.Mode = SearchBar.Mode.Empty
 
     def print_base(self):
         for line, option in enumerate(self.options):
             print(f"{self.get_line(line)}{option}")
 
-    def get_line(self, line) -> str:
-        digits = len(str(self.line_count))
+    def get_line(self, line, line_max=None) -> str:
+        if line_max is None:
+            line_max = self.line_count
+        digits = len(str(line_max))
         return f"[{str(line).zfill(digits)}] "
 
     def search(self):
@@ -44,34 +61,191 @@ class SearchBar:
             self.exit()
         elif key == "backspace":
             self.remove_char()
+        elif key == "space":
+            self.add_char(" ")
+        elif key == "tab":
+            self.add_char("\t")
+
+
+    def set_mode(self, new_mode: SearchBar.Mode):
+        self.mode = new_mode
+
+    def flair(self) -> Optional[str]:
+        if self.flair_text is None:
+            return None
+        if self.flair_text == "":
+            return None
+        return f"{self.flair_color}{self.flair_text}{colorama.Fore.RESET}"
+
+    def add_flair_char(self, key):
+        self.flair_text += key
+
+    def remove_flair_char(self):
+        if not self.flair_text:
+            return
+        self.flair_text = self.flair_text[:-1]
 
     def add_char(self, key):
+        if self.mode == SearchBar.Mode.Empty:
+            if key == "\\":
+                self.set_mode(SearchBar.Mode.Escaping)
+                self.flair_text = f"\\"
+                self.flair_color = colorama.Fore.YELLOW
+                self.update_first_line()
+                return
+            else:
+                self.set_mode(SearchBar.Mode.Standard)
+                self.add_char_standard(key)
+                return
+        elif self.mode == SearchBar.Mode.Escaping:
+            if (key == "r") or (key == "R"):
+                self.set_mode(SearchBar.Mode.Regex)
+                self.flair_text = f"Regex: "
+                self.flair_color = colorama.Fore.GREEN
+                self.update_first_line()
+            elif key == "\\":
+                self.set_mode(SearchBar.Mode.Standard)
+                self.flair_text = None
+                self.flair_color = colorama.Fore.WHITE
+                self.add_char_standard("\\")
+            elif (key == " ") or (key == "\t"):
+                return
+            else:
+                self.set_mode(SearchBar.Mode.InvalidMode)
+                self.flair_text = f"\\{key}"
+                self.flair_color = colorama.Fore.RED
+                self.update_first_line()
+            return
+        elif self.mode == SearchBar.Mode.InvalidMode:
+            if (key == " ") or (key == "\t"):
+                return
+            self.add_flair_char(key)
+            if self.flair_text.lower() == "regex":
+                self.flair_text = f"Regex: "
+                self.flair_color = colorama.Fore.GREEN
+                self.update_first_line()
+        elif self.mode == SearchBar.Mode.Standard:
+            self.add_char_standard(key)
+        elif self.mode == SearchBar.Mode.Regex:
+            self.add_char_regex(key)
+        elif self.mode == SearchBar.Mode.RegexInvalid:
+            self.add_char_regex(key)
+
+    def add_char_standard(self, key: str):
         self.search_line += key
+        search_pattern = self.search_line.replace("\\", "\\\\")
+        for special_char in [".", "^", "$", "?", "*", "+", "{", "}", "[", "]", "|", "(", ")"]:
+            search_pattern = search_pattern.replace(special_char, "\\" + special_char)
         temp = []
+        pattern = re.compile(search_pattern)
         for item in self.options_slice:
-            # TODO: Compile pattern to use literal no matter what
-            if re.search(self.search_line, item) is not None:
+            if re.search(pattern, item) is not None:
                 temp.append(item)
         self.options_slice = temp
         self.update_display()
 
-    def remove_char(self):
-        if len(self.search_line) == 0:
+    def add_char_regex(self, key: str):
+        self.search_line += key
+        temp = []
+        try:
+            pattern = re.compile(self.search_line)
+            for item in self.options_slice:
+                if re.search(pattern, item) is not None:
+                    temp.append(item)
+        except re.error:
+            self.set_mode(SearchBar.Mode.RegexInvalid)
+            self.search_text_color = colorama.Fore.RED
+            self.update_first_line()
             return
+        self.search_text_color = colorama.Fore.WHITE
+        self.options_slice = temp
+        self.update_display()
+
+    def remove_char(self):
+        if self.mode == SearchBar.Mode.Empty:
+            return
+        if self.mode == SearchBar.Mode.Escaping:
+            self.remove_char_escaping()
+            return
+        if self.mode == SearchBar.Mode.InvalidMode:
+            self.remove_char_invalid()
+            return
+        if self.mode == SearchBar.Mode.Standard:
+            self.remove_char_standard()
+            return
+        if self.mode == SearchBar.Mode.Regex:
+            self.remove_char_regex()
+            return
+        if self.mode == SearchBar.Mode.RegexInvalid:
+            self.remove_char_regex()
+
+    def set_search_empty(self):
+        self.search_line = ""
+        self.options_slice = self.options
+        self.flair_text = ""
+        self.flair_color = colorama.Fore.WHITE
+        self.set_mode(SearchBar.Mode.Empty)
+        self.update_display()
+
+    def remove_char_escaping(self):
+        self.set_search_empty()
+
+    def remove_char_invalid(self):
+        self.remove_flair_char()
+        if self.flair_text.lower() in ["regex", "r"]:
+            self.flair_text = f"Regex: "
+            self.flair_color = colorama.Fore.GREEN
+            self.update_first_line()
+            return
+        if self.flair_text == "\\":
+            self.set_mode(SearchBar.Mode.Escaping)
+            self.flair_color = colorama.Fore.YELLOW
+            self.update_first_line()
+            return
+
+    def remove_char_standard(self):
+        if len(self.search_line) == 1:
+            self.set_search_empty()
+            return
+        self.search_line = self.search_line[:-1]
+        search_pattern = self.search_line.replace("\\", "\\\\")
+        for special_char in [".", "^", "$", "*", "+", "{", "}", "[", "]", "|", "(", ")"]:
+            search_pattern = search_pattern.replace(special_char, "\\" + special_char)
+        temp = []
+        pattern = re.compile(search_pattern)
+        for item in self.options_slice:
+            if re.search(pattern, item) is not None:
+                temp.append(item)
+        self.options_slice = temp
+        self.update_display()
+
+    def remove_char_regex(self):
+        if len(self.search_line) == 0:
+            self.flair_text = "\\"
+            self.flair_color = colorama.Fore.YELLOW
+            self.set_mode(SearchBar.Mode.Escaping)
+            self.update_first_line()
         if len(self.search_line) == 1:
             self.search_line = ""
             self.options_slice = self.options
+            self.search_text_color = colorama.Fore.WHITE
             self.update_display()
             return
-        if len(self.search_line) > 1:
-            self.search_line = self.search_line[:-1]
-            temp = []
+        self.search_line = self.search_line[:-1]
+        temp = []
+        try:
+            pattern = re.compile(self.search_line)
             for item in self.options:
-                # TODO: Compile pattern to use literal no matter what
-                if re.search(self.search_line, item) is not None:
+                if re.search(pattern, item) is not None:
                     temp.append(item)
-            self.options_slice = temp
-            self.update_display()
+        except re.error:
+            self.set_mode(SearchBar.Mode.RegexInvalid)
+            self.search_text_color = colorama.Fore.RED
+            self.update_first_line()
+            return
+        self.search_text_color = colorama.Fore.WHITE
+        self.options_slice = temp
+        self.update_display()
 
     def reset_cursor(self):
         if self.cursor <= 0:
@@ -79,46 +253,61 @@ class SearchBar:
         print(f"{colorama.Cursor.UP(self.cursor)}",end="\r")
         self.cursor = 0
 
-    def clear_line(self):
-        print(f"{colorama.ansi.clear_line()}",end="\r")
+    def clear_line(self) -> str:
+        return f"{colorama.ansi.clear_line()}"
 
-    def cursor_down(self):
-        self.cursor += 1
-        print(f"{colorama.Cursor.DOWN(1)}", end="\r")
+    def cursor_down(self) -> str:
+        return f"{colorama.Cursor.DOWN(1)}"
 
     def update_display(self):
+        self.update_list()
+        self.update_first_line()
+
+    def update_list(self, start: int = 0, length: Optional[int] = None):
+        if start > len(self.options_slice):
+            return
+        if length is None:
+            length = self.max_opts
+        end: int = min(start+length, len(self.options_slice))
+        if length <= 0:
+            if self.printed_options:
+                self.clear_options()
+            return
+        options_to_print = self.options_slice[start:end]
+        if self.printed_options == options_to_print:
+            return
+        self.clear_options()
+        print_str = self.cursor_down()
+        self.cursor += 1
+        # TODO: change i to correspond to placement in options
+        length = end-start
+        if options_to_print:
+            print_str += "\n".join([f"{self.get_line(i+start+1, length)}{line}" for i, line in enumerate(options_to_print)]) + "\n"
+        print(print_str, end="\r")
+        self.cursor += len(options_to_print)
+        self.printed_options = options_to_print
+
+    def clear_options(self):
+        # Clear current lines
+        self.reset_cursor()
+        print_str = self.cursor_down()
+        self.cursor += 1
+        print_str += f"{colorama.ansi.clear_line()}\n" * len(self.printed_options)
+        print(print_str, end="\r")
+        self.cursor += len(self.printed_options)
+        self.reset_cursor()
+
+    def update_first_line(self):
+        self.reset_cursor()
         first_line = self.search_line
         # Write search statement
-        if first_line == "":
-            first_line = "Search..."
-        self.reset_cursor()
-        self.clear_line()
-        print(f"{first_line}")
-        self.cursor += 1
-        # Done if there are no changes to the options
-        if self.printed_options == self.options_slice:
+        if self.mode == SearchBar.Mode.Empty:
+            print(f"{self.clear_line()}\rSearch...", end="\r")
             return
-        # Clear current lines
-        print_str = f"{colorama.ansi.clear_line()}\n" * len(self.printed_options)
-        print(print_str,end="\r")
-        self.cursor += len(self.printed_options)
-        # for i in range(len(self.printed_options)):
-        #     self.clear_line()
-        #     self.cursor_down()
-        # Write new lines below search
-        self.reset_cursor()
-        self.cursor_down()
-        # TODO: change i to correspond to placement in options
-        if self.options_slice:
-            print_str = "\n".join([f"{self.get_line(i)}{line}" for i, line in enumerate(self.options_slice)]) + "\n"
+        if self.flair() is not None:
+            print(f"{self.clear_line()}\r", end=f"{self.flair()}{self.search_text_color}{first_line}{colorama.Fore.RESET}")
         else:
-            print_str = ""
-        print(print_str,end="\r")
-        # for i, line in enumerate(self.options_slice):
-        #     print(f"{self.get_line(i)}{line}")
-        #     self.cursor += 1
-        self.cursor += len(self.options_slice)
-        self.printed_options = self.options_slice
+            print(f"{self.clear_line()}\r", end=f"{self.search_text_color}{first_line}{colorama.Fore.RESET}")
 
     def exit(self):
         self.reset_cursor()
@@ -197,8 +386,8 @@ async def main():
     # Note: only works in Pycharm with menu->run->edit configurations...->configuration->execution->Emulate terminal output [enabled]
     # Initialize colorama
     colorama.init()
-    foods = ["Salad", "Sandwich", "Soup"]
-    # wordle_words = open("valid-wordle-words.txt", "r").read().splitlines()[::140]
+    foods = ["Sal.ad", "Sand\\wich", "So?up"]
+    # wordle_words = open("valid-wordle-words.txt", "r").read().splitlines()[::]
     # menu = Menu(foods)
     # selection = await menu.select()
     # print(f"Selected {foods[selection]}!")
